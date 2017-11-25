ffe417657df6fe8ae50c806b2178b1336def97c7
Andrew Thomson <Thomson.w.Andrew@gmail.com>
2017-11-25 11:05:52 -0700

    run prettier.

--- src/components/edit-product.js
+++ src/components/edit-product.js
@@ -1,66 +1,58 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import glamorous from 'glamorous'
+import React from "react";
+import { connect } from "react-redux";
+import glamorous from "glamorous";
 
-import Popover from 'material-ui/Popover';
-import RaisedButton from 'material-ui/RaisedButton';
+import Popover from "material-ui/Popover";
+import RaisedButton from "material-ui/RaisedButton";
 
 const Cell = glamorous.div({
-  height: 'inherit',
-  width: 'inherit',
-})
-
-
+  height: "inherit",
+  width: "inherit"
+});
 
 class EditProduct extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       open: false
-    }
+    };
 
-    this.handleTouchTap = this.handleTouchTap.bind(this)
-    this.handleRequestClose = this.handleRequestClose.bind(this)
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
   }
 
-  handleTouchTap = (event) => {
+  handleTouchTap = event => {
     event.preventDefault();
-    console.log(event)
+    console.log(event);
     this.setState({
       open: true,
-      anchorEl: event.currentTarget,
+      anchorEl: event.currentTarget
     });
   };
 
   handleRequestClose = () => {
     this.setState({
-      open: false,
+      open: false
     });
   };
 
   render() {
     return (
       <Cell>
-        <RaisedButton
-          onClick={this.handleTouchTap}
-          label={'More'}
-        >
+        <RaisedButton onClick={this.handleTouchTap} label={"More"}>
           <Popover
             open={this.state.open}
             anchorEl={this.state.anchorEl}
-            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+            anchorOrigin={{ horizontal: "left", vertical: "bottom" }}
+            targetOrigin={{ horizontal: "left", vertical: "top" }}
             onRequestClose={this.handleRequestClose}
           >
-            <div>
-              {'Change Price'}
-            </div>
+            <div>{"Change Price"}</div>
           </Popover>
         </RaisedButton>
       </Cell>
     );
   }
 }
-
 
 export default connect()(EditProduct);
